6147bf6f7b3bd0f91894cbf826dd14c76c398fbe
saif <abutalha666124@gmail.com>
2023-04-20 22:58:34 +0600

    'solve'

--- cv-back-end/index.js
+++ cv-back-end/index.js
@@ -4,7 +4,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000 ;
 
-//middle ware 
+//middle ware  
 app.use(cors())
 app.use(express.json())
 
